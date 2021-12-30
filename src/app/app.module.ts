1f173f3ad58dc441486221e2299cfe8bc0a4327b
Alexander Huxel <alexanderhuxel@gmail.com>
2021-12-30 19:44:01 +0100

    logic update for the form and style update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +8,18 @@ import { BoardComponent } from './components/board/board.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HelpComponent } from './components/help/help.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 @NgModule({
   declarations: [AppComponent, BoardComponent, HelpComponent, AddTaskComponent],
   imports: [
@@ -17,6 +28,16 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     BrowserAnimationsModule,
     MatSidenavModule,
     FontAwesomeModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.scss']
+  styleUrls: ['./add-task.component.scss'],
 })
 export class AddTaskComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  taskForm = new FormGroup({
+    title: new FormControl('Nancy', Validators.minLength(2)),
+    category: new FormControl('Drew'),
+    description: new FormControl('Drew'),
+    date: new FormControl('Drew'),
+    importance: new FormControl('Drew'),
+  });
 
-  ngOnInit(): void {
+  task = {
+    title: '',
+    category: '',
+    description: '',
+    date: '',
+    importance: '',
+    done: false,
+  };
+
+  test() {
+    console.log(this.task);
   }
 
+  ngOnInit(): void {}
 }
