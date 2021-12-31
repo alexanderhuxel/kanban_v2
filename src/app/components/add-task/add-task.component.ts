5a74f09333847a092e77bca092d54cfe0ff6e833
Alexander Huxel <alexanderhuxel@gmail.com>
2021-12-31 14:51:40 +0100

    removed forms modules

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Task } from 'src/models/task';
 @Component({
   selector: 'app-add-task',
