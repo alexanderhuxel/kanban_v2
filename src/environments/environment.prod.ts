49a77ce80e2162f7f615eec8c49db2ca1b5655d1
Alexander Huxel <alexanderhuxel@gmail.com>
2022-01-02 18:03:49 +0100

    enviremont update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'kanbanboard-11cda',
+    appId: '1:543027336108:web:9a114805841d86f6f92009',
+    storageBucket: 'kanbanboard-11cda.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBPYdKQ8xC-FaUXPQqcw0XGWUsoluwUIC4',
+    authDomain: 'kanbanboard-11cda.firebaseapp.com',
+    messagingSenderId: '543027336108',
+  },
   production: true
 };
