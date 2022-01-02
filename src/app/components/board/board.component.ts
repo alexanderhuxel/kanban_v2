add13c487c974ac094ae609f45264afab6d72fc2
Alexander Huxel <alexanderhuxel@gmail.com>
2022-01-02 18:04:21 +0100

    update the board and implement a function with create a new col in the database with an empty object in it

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { DatabaseService } from 'src/app/services/database.service';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+  doc,
+  docData,
+  deleteDoc,
+  updateDoc,
+  collectionChanges,
+  DocumentReference,
+  setDoc,
+} from '@angular/fire/firestore';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  constructor(public database: DatabaseService, private firestore: Firestore) {}
 
-  constructor() { }
+  object = {};
 
-  ngOnInit(): void {
+  addNewColumn() {
+    // TODO: create dialog for name of column
+    const nestedPath = '/teamBoardAlex/board/test/';
+    const databasePath: any = collection(this.firestore, nestedPath);
+    return addDoc(databasePath, this.object);
   }
 
+  faPlus = faPlus;
+  ngOnInit(): void {}
 }
