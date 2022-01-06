15a32680c3b4d70d9309f570f9aa7b5e13fac3cb
Alexander Huxel <alexanderhuxel@gmail.com>
2022-01-06 21:02:30 +0100

    updated board-component

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,36 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { DatabaseService } from 'src/app/services/database.service';
-import {
-  Firestore,
-  addDoc,
-  collection,
-  collectionData,
-  doc,
-  docData,
-  deleteDoc,
-  updateDoc,
-  collectionChanges,
-  DocumentReference,
-  setDoc,
-} from '@angular/fire/firestore';
+import { Task } from 'src/models/task';
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
-  constructor(public database: DatabaseService, private firestore: Firestore) {}
-
-  object = {};
-
-  addNewColumn() {
-    // TODO: create dialog for name of column
-    const nestedPath = '/teamBoardAlex/board/test/';
-    const databasePath: any = collection(this.firestore, nestedPath);
-    return addDoc(databasePath, this.object);
-  }
-
+  constructor(public database: DatabaseService) {}
   faPlus = faPlus;
-  ngOnInit(): void {}
+  object = {};
+  ToDo = this.database
+    .getBoard('To Do')
+    .subscribe((result) => console.log(result));
+
+  inProgress = this.database
+    .getBoard('in Progress')
+    .subscribe((result) => console.log(result));
+
+  Done = this.database
+    .getBoard('Done')
+    .subscribe((result) => console.log(result));
+
+  ngOnInit(): void {
+    console.log(this.ToDo, this.inProgress, this.Done);
+  }
 }
