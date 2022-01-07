d6b3ae3e6cde6f7234852164e7387c562c0689a5
Alexander Huxel <alexanderhuxel@gmail.com>
2022-01-07 13:23:11 +0100

    update add-task compontent

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,18 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
 import { DatabaseService } from 'src/app/services/database.service';
-
-// import { Observable } from 'rxjs';
 import { Task } from 'src/models/task';
-
-// interface Item {
-//   title: string;
-//   category: string;
-//   description: string;
-//   date: string;
-//   importance: string;
-//   done: boolean;
-// }
 
 @Component({
   selector: 'app-add-task',
@@ -21,7 +9,6 @@ import { Task } from 'src/models/task';
 export class AddTaskComponent implements OnInit {
   task: Task = new Task();
 
-  constructor(private firestore: Firestore, public database: DatabaseService) {}
-
+  constructor(public database: DatabaseService) {}
   ngOnInit(): void {}
 }
