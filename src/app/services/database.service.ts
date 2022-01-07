76561c41cebe821921b9eea2cd0e69cb71583e33
Alexander Huxel <alexanderhuxel@gmail.com>
2022-01-07 13:22:36 +0100

    updatet database-service

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -10,12 +10,17 @@ import {
 import { collection, doc } from 'firebase/firestore';
 import { Task } from 'src/models/task';
 
+// TODO: add logic to chaneg Row dynamicli
+
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseService {
   constructor(private firestore: Firestore) {}
   Task = new Task();
+  firstRowArray: any = [];
+  secondRowArray: any = [];
+  thirdRowArray: any = [];
 
   getBoard(path: string) {
     const collectionRef = collection(
@@ -36,7 +41,14 @@ export class DatabaseService {
       this.firestore,
       'teamBoardAlex/board/' + path
     );
-    return addDoc(collectionRef, Task.objectToJSON());
+    if (this.secondRowArray.length == 3 && path == 'in Progress') {
+      alert(
+        'The Row you choosed is currently filled with 3 Task, change your Selection or reorder your Board!'
+      );
+      return;
+    } else {
+      return addDoc(collectionRef, Task.objectToJSON());
+    }
   }
 
   removeTask(Task: Task, id: string) {
@@ -51,4 +63,20 @@ export class DatabaseService {
       text: Task.description,
     });
   }
+
+  firstRow = this.getBoard('To Do').subscribe((result) => {
+    console.log(result);
+    this.firstRowArray = result;
+    console.log(this.firstRowArray.length);
+  });
+
+  SecondRow = this.getBoard('in Progress').subscribe((result) => {
+    console.log(result);
+    this.secondRowArray = result;
+  });
+
+  ThirdRow = this.getBoard('Done').subscribe((result) => {
+    console.log(result);
+    this.thirdRowArray = result;
+  });
 }
